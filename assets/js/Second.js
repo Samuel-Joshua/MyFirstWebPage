e661f2d20773943afd76f50c96f83e6a1918161a
Samuel <sammy.martis@gmail.com>
2021-05-05 20:04:01 +0200

    initial commit

---
+++ assets/js/Second.js
@@ -0,0 +1,11 @@
+// JavaScript source code
+
+
+var $a = function stateChange()
+{
+    setTimeout(function () {
+        window.location.href = "index.html";
+    }, 5000);
+} 
+
+$a.call();
---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+﻿var b = 1;
+function thisFunction($)
+{
+    var c = 2;
+
+    if (b%c == 0) {
+        console.log("You clicked an odd amount of times!");
+    } else {
+        console.log("You clicked an even amount of times!");
+    }
+    b += 1;
+
+
+    if (b > 10)
+    {
+       window.location.href = "FacePage.html";
+    }
+}
+
+document.getElementById("Button1").addEventListener("click", thisFunction);
+document.write("Loading webpage completed");
+window.alert("Alert!");
